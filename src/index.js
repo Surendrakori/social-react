6813efd26d42d1eba18c067229467275ebbac888
surendra <surendrakori@lerostech.com>
2024-03-14 17:17:17 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { UserContextProvider } from './UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <UserContext.Provider>
+    <UserContextProvider>
     <App />
-    </UserContext.Provider>
+    </UserContextProvider>
 );
 
 
