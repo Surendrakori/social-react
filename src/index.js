460d0a61c3f116b2589f7b48cea974b2d3b4c6d6
surendra <surendrakori@lerostech.com>
2024-03-14 18:46:02 +0530

    deleted

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { UserContextProvider } from './UserContext';
+import { UserContext } from './Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <UserContextProvider>
-    <App />
-    </UserContextProvider>
+
 );
 
 
