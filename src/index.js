f4ad38179dba2e429c3819024b02eb89d9c71eed
surendra <surendrakori@lerostech.com>
2024-03-17 12:36:47 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { UserContext } from './Context';
+// import { UserContext } from './Context';
+import Usercontext from './components/UserContext/Usercontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+    <>
+      <Usercontext>
+      <App/>
+      </Usercontext>
+    </>
 );
 
 
